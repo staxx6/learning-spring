90742e94da7997ae8cd74d10b56bb97b9b0fedb7
Simon Oczadly <staxx@hotmail.de>
2019-01-14 22:03:58 +0100

    finished web-customer-tracker saving new customer

---
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer", Customer.class);
+		
+		return theQuery.getResultList();
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		currentSession.save(theCustomer);
+	}
+}
