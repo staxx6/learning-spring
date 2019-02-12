c35c899cc24c21d5a6ef695119c79c8600212260
Simon Oczadly <staxx@hotmail.de>
2019-02-13 00:15:22 +0100

    doing aop; finished @Pointcut with logic

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,6 +18,31 @@ public class MyDemoLoggingAspect {
 	//@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))") // any package method with 0-oo params
 	@Before("execution(* com.luv2code.aopdemo.dao.*.*(com.luv2code.aopdemo.Account, *))")
 	public void beforeAddAccountAdvice() {
-		System.out.println("\n=====> Executing @Before advice on addAccount()");
+//		System.out.println("\n=====> Executing @Before advice on addAccount()");
+	}
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(com.luv2code.aopdemo.Account, *))")
+	public void aopStuffVar() {}
+	
+	@Pointcut("execution(* *.fantasy(..))") // nothing match - for logic only 
+	public void aopStuffVarTwo() {}
+	
+	//@Pointcut("aopStuffVar() ! (getter() || setter())")
+	//public void aopStuffVarThree() {}
+	
+	@Before("aopStuffVar()")
+	public void oneStuff() {
+		System.out.println("\n=====> Executing @Before oneStuff");
+	}
+	
+	@Before("aopStuffVar()")
+	public void twoStuff() {
+		System.out.println("\n=====> Executing @Before twoStuff");
+	}
+	
+	//@Before("aopStuffVar() ! (getter() || setter())")
+	@Before("aopStuffVar() ! aopStuffVarTwo()") // || or && possible too // ! DONT MATCH
+	public void threeStuff() {
+		System.out.println("\n=====> Executing @Before threeStuff");
 	}
 }
