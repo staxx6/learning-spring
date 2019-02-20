5a85ed02f7dd627e9967ecd0121fd8ce2c7ff99f
Simon Oczadly <staxx@hotmail.de>
2019-02-20 19:45:21 +0100

    starting with spring security

---
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication().withUser(users.username("john").password("john").roles("EMPLOYEE"));
+        auth.inMemoryAuthentication().withUser(users.username("mary").password("mary").roles("MANAGER"));
+        auth.inMemoryAuthentication().withUser(users.username("admin").password("admin").roles("ADMIN"));
+    }
+}
