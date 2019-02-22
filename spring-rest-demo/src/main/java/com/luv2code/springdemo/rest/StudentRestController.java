0c1086efb311ae591584f0edeef352ffe2ad87ee
Simon Oczadly <staxx@hotmail.de>
2019-02-22 23:07:16 +0100

    doing REST

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo.rest;
+
+import com.luv2code.springdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Erst", "Zweit"));
+        students.add(new Student("Wasd", "Lep"));
+        students.add(new Student("Last", "First"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return students.get(studentId);
+    }
+}
---
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: staxx6
+  Date: 22.02.19
+  Time: 22:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>wubba lubba dub dub index</title>
+</head>
+<body>
+<h1>wubba lubba dub dub index-b</h1>
+</body>
+</html>
